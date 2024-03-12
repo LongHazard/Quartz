cfe6c4bd1c7c3aab52c3b148231127848b2314cd
LongHazard <nguyenlongvan2011@gmail.com>
2024-03-12 18:05:18 +0700

    long

--- src/main/java/com/example/quarzt/configuration/QuartzConfig.java
+++ src/main/java/com/example/quarzt/configuration/QuartzConfig.java
@@ -1,13 +1,16 @@
-//package com.example.quarzt.configuration;
-//
-//import com.example.quarzt.job.JobTest;
-//import org.quartz.*;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class QuartzConfig {
-//
+package com.example.quarzt.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class QuartzConfig {
+
 //  @Bean
 //  public JobDetail helloJobDetail() {
 //    return JobBuilder.newJob(JobTest.class)
@@ -24,4 +27,23 @@
 //          .withSchedule(CronScheduleBuilder.cronSchedule("0 * * ? * *")) // Chạy vào đầu mỗi phút
 //          .build();
 //  }
-//}
+
+  @Autowired
+  private DataSource dataSource;
+
+  @Bean
+  public SchedulerFactoryBean scheduler() {
+    SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
+
+    Properties properties = new Properties();
+    properties.setProperty("org.quartz.scheduler.instanceName", "MY_INSTANCE_NAME");
+    properties.setProperty("org.quartz.scheduler.instanceId", "INSTANCE_ID_01");
+    properties.put("org.quartz.jobStore.driverDelegateClass", "org.quartz.impl.jdbcjobstore.PostgreSQLDelegate");
+    schedulerFactory.setQuartzProperties(properties);
+    schedulerFactory.setDataSource(dataSource);
+
+    schedulerFactory.setWaitForJobsToCompleteOnShutdown(true);
+
+    return schedulerFactory;
+  }
+}
